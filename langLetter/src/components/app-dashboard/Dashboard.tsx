1099b38a0d6139a0af0d5fa741fa23bda1f6a3b0
ANNA124kr <ann.krav4uk13@gmail.com>
2025-02-28 16:33:34 +0200

    initial files

---
+++ langLetter/src/components/app-dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface DashboardProps {
+  title: string;
+  description: string;
+}
+
+const Dashboard: React.FC<DashboardProps> = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Dashboard;
